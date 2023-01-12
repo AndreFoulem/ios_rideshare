36f4c2222da2e6109f2a5cd897a41683841020a4
AndreFoulem <timmycasual@icloud.com>
2023-01-11 20:10:40 -0800

    Create project folder architecture

---
+++ robodrive/Core/Views/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  robodrive
+//
+//  Created by AndreMacBook on 2023-01-11.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
