1d6280d05a26e64dd4a16527e5df5040faf48ae5
AndreFoulem <timmycasual@icloud.com>
2023-01-11 20:00:53 -0800

    Create project

---
+++ robodrive/robodriveApp.swift
@@ -0,0 +1,17 @@
+//
+//  robodriveApp.swift
+//  robodrive
+//
+//  Created by AndreMacBook on 2023-01-11.
+//
+
+import SwiftUI
+
+@main
+struct robodriveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
