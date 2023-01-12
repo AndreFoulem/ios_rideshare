47194bebdbfb95bc1b56fc6df0123228a51194dc
AndreFoulem <timmycasual@icloud.com>
2023-01-11 20:42:12 -0800

    Add RobodriveMapViewRepresentable.swift

---
+++ robodrive/Core/Views/RobodriveMapViewRepresentable.swift
@@ -0,0 +1,53 @@
+//
+//  RobodriveMapViewRepresentable.swift
+//  robodrive
+//
+//  Created by AndreMacBook on 2023-01-11.
+//
+
+
+import SwiftUI
+import MapKit
+
+struct RobodriveMapViewRepresentable: UIViewRepresentable {
+  let mapView = MKMapView()
+  
+  func makeUIView(context: Context) -> some UIView {
+    mapView.isRotateEnabled = false
+    mapView.showsUserLocation = true
+    mapView.userTrackingMode = .follow
+    
+    return mapView
+  }
+  
+  func updateUIView(_ uiView: UIViewType, context: Context) {
+      
+  }
+  
+  func makeCoordinator() -> MapCoordinator {
+    return MapCoordinator(parent: self)
+  }
+//  func makeCoordinator() -> () {
+//
+//  }
+//  func makeUIView(context: Context) -> MKMapView {
+//    return mapView
+//  }
+//
+//  func updateUIView(_ uiView: MKMapView, context: Context) {
+//      // This can be used to update the map view, if necessary.
+//      // For example, if you need to change the region of the map, you can set the region property of the map view here.
+//  }
+  
+}// RobodriveMapViewRepresentable
+
+extension RobodriveMapViewRepresentable{
+  class  MapCoordinator: NSObject, MKMapViewDelegate {
+    let parent: RobodriveMapViewRepresentable
+    
+    init(parent: RobodriveMapViewRepresentable) {
+      self.parent = parent
+      super.init()
+    }
+  }
+}
